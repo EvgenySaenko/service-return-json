8727fe136bc3b7d3c8f397a7d8419c15f1701702
Evgeny Saenko <storn84@mail.ru>
2020-10-10 16:58:54 +0300

    add service-return-json

---
+++ src/main/java/com/servicereturnsjson/ServiceReturnsJsonApplication.java
@@ -0,0 +1,13 @@
+package com.servicereturnsjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceReturnsJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceReturnsJsonApplication.class, args);
+	}
+
+}
